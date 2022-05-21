6debbbc99369398ff67e02ced7ed09ddfceea6cc
GeorgeOrd <georgesmithord@hotmail.com>
2022-05-21 11:45:31 -0500

    Cambio en funcionalidad del app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-#esta es una priamide
-#El pene del cerdo es de forma de un tornillo
-for a in range(0,10):
-    
-    print("*\n" * a)
+# revisa numeros pares
+# El pene del cerdo es de forma de un tornillo
+for a in range(0, 10):
+
+    if (a % 2 == 0):
+        print("Es un numero par")
+    else : 
+        print("numero impar")
